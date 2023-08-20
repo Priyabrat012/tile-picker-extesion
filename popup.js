c7e6e4d442003b325ff4b2a4b8a12e6505dbcfc2
prio <priyabrat012@gmail.com>
2023-08-20 07:38:48 +0530

    final version title picler ext

--- popup.js
+++ popup.js
@@ -1,37 +1,13 @@
-const btn = document.querySelector(".changeColorBtn")
-
-const colorGrid = document.querySelector('.colorGrid');
-
-const colorValue = document.querySelector('.colorValue');
-
-btn.addEventListener("click",async () => {
-  let [tab] = await chrome.tabs.query({active: true,currentWindow: true})
-  console.log(tab)
-  chrome.scripting.executeScript({
-    target: {tabId:tab.id},
-    function: pickColor,
-  }, async (injeectionResult) => {
-        const [data] = injeectionResult;
-        if(data.result){
-            const color = data.result.sRGBHex;
-            colorGrid.style.background =color
-            colorValue.innerText = color
-            await navigator.clipboard.writeText(color);
-            
-        }
-  })
-})
-
-async function pickColor() {
-   
-try {
-    const eyeDropper = new EyeDropper();
-    return  await eyeDropper.open();
-   
-
-} catch (error) {
-    console.error(error);
-}
-
-
-}
+document.addEventListener('DOMContentLoaded', function() {
+    const fetchTitleButton = document.getElementById('fetchTitleButton');
+    const titleContainer = document.getElementById('titleContainer');
+  
+    fetchTitleButton.addEventListener('click', async () => {
+        // get chrome apis // tab info 
+      const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+     // console.log(tab);
+      const tabTitle = tab.title;
+      titleContainer.textContent = `Current Tab Title: ${tabTitle}`;
+    });
+  });
+  
